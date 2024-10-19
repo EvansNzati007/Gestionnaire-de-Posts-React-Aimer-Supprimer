a2236b59dc944aecd87bfa3e7ba1c92a24102eae
EvansNzati007 <evansnzati@gmail.com>
2024-10-19 15:41:08 +0100

    Mis à jour de la description des Posts

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,30 @@ function App() {
 
   const [posts, setPosts] = useState([
     {
-        id :1,
-        titre: "Nzati Doumbi Evans",
-        description:
-        "voici un exemple de description de texte placer ici meme",
-        liker: false
-    },
-    {
-        id :2,
-        titre: "Leo Messi",
-        description:
-        "voici un exemple de description de texte placer ici meme",
-        liker: false
-    },
-    {
-        id :3,
-        titre: "Jeff Bezos",
-        description:
-        "voici un exemple de description de texte placer ici meme",
-        liker: false
-    },
-    {
-        id :4,
-        titre: "Aliko Dangote",
-        description:
-        "voici un exemple de description de texte placer ici meme",
-        liker: false
-    },
+      "id": 1,
+      "titre": "Nzati Doumbi Evans",
+      "description": "Nzati Doumbi Evans est un entrepreneur gabonais visionnaire, reconnu pour son engagement dans le développement technologique en Afrique centrale. Il est à l'origine de plusieurs startups innovantes dans le secteur des fintechs.",
+      "liker": false
+  },
+  {
+      "id": 2,
+      "titre": "Leo Messi",
+      "description": "Leo Messi est un footballeur de renommée mondiale, souvent considéré comme l'un des meilleurs joueurs de tous les temps. Son parcours exceptionnel au FC Barcelone et sa transition réussie à l'Inter Miami inspirent des millions de fans.",
+      "liker": false
+  },
+  {
+      "id": 3,
+      "titre": "Jeff Bezos",
+      "description": "Jeff Bezos est le fondateur d'Amazon, la plus grande plateforme de commerce en ligne au monde. En plus de ses réalisations dans le domaine du e-commerce, il est également un pionnier dans l'exploration spatiale avec sa société Blue Origin.",
+      "liker": false
+  },
+  {
+      "id": 4,
+      "titre": "Aliko Dangote",
+      "description": "Aliko Dangote est un magnat des affaires nigérian, connu pour être l'homme le plus riche d'Afrique. Son empire industriel s'étend dans les secteurs du ciment, du sucre, et du pétrole, jouant un rôle clé dans l'économie africaine.",
+      "liker": false
+  }
+  
 ]);
 
 const liker = (data) => {
